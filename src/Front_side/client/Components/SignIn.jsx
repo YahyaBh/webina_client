174a4169672a812958be6c9453428c1d11eee9a6
Yahya Bouhsine <81886762+YahyaBh@users.noreply.github.com>
2023-01-18 13:48:27 +0100

    login

--- src/Front_side/client/Components/SignIn.jsx
+++ src/Front_side/client/Components/SignIn.jsx
@@ -1,5 +1,7 @@
 import axios from 'axios';
 import React, { useState, useEffect, useContext } from 'react'
+import { AiOutlineGoogle } from 'react-icons/ai';
+import { Navigate } from 'react-router-dom';
 import { AuthContext } from '../../UserContext';
 
 const SignIn = () => {
@@ -8,41 +10,62 @@ const SignIn = () => {
     const [emailInput, setEmailInput] = useState('');
     const [passwordInput, setPasswordInput] = useState('');
 
-    const submitForm = function (e) {
+
+
+    const submitForm = async (e) => {
         e.preventDefault();
 
         const formData = new FormData()
 
-        formData.append('email', emailInput)
-        formData.append('password', passwordInput)
+        try {
+            formData.append('email', emailInput)
+            formData.append('password', passwordInput)
 
-        axios.post('http://localhost:8000/api/signin', formData)
-            .then(res => {
-                console.log(res.data)
-            })
+            axios.post('http://localhost:8000/api/signin', formData)
+                .then(res => {
+                    console.log(res.data)
+                    Navigate('/')
+                })
+        } catch (error) {
+            if (error.response.status === 422) {
+                console.log(error.response.data.errors);
+            }
+
+        }
+
+        // const auth = useContext(AuthContext);
+        // const token = auth.token;
     }
 
-    const auth = useContext(AuthContext);
-    const token = auth.token;
-
-
     return (
-        <div>
-
-            <form onSubmit={submitForm}>
-
-                <input type="email" name="email" placeholder="Email" value={emailInput} onChange={(e) => setEmailInput(e.target.value)} />
-
-                <input type="password" name="password" placeholder="Password" value={passwordInput} onChange={(e) => setPasswordInput(e.target.value)} />
+        <div className='app__signin'>
 
 
-                <button type='submit'>Sign In</button>
+            <div className='app__signin__form'>
+                <form onSubmit={submitForm}>
+                    <h2>SIGN IN</h2>
 
 
-            </form>
+                    <input type="email" name="email" placeholder="Email" value={emailInput} onChange={(e) => setEmailInput(e.target.value)} />
 
+                    <input type="password" name="password" placeholder="Password" value={passwordInput} onChange={(e) => setPasswordInput(e.target.value)} />
+
+
+                    <button type="submit" className='app__google__signup'><AiOutlineGoogle /></button>
+
+                    <button type='submit'>Sign In</button>
+
+                    <p>Don't Have An Account Yet ? <a href='/signup'>Sign Up</a></p>
+
+                </form>
+
+            </div>
+
+
+            <div className='app__signin__form__bg'></div>
         </div>
     )
 }
+
 
 export default SignIn
--- src/Front_side/client/Components/SignUp.jsx
+++ src/Front_side/client/Components/SignUp.jsx
@@ -1,35 +1,50 @@
 import axios from 'axios';
 import React, { useState } from 'react'
+import { AiOutlineGoogle } from 'react-icons/ai';
+
 
 const SignUp = () => {
 
     const [name, setName] = useState('');
     const [emailInput, setEmailInput] = useState('');
     const [passwordInput, setPasswordInput] = useState('');
+    const [verify_passwordInput, setVerPasswordInput] = useState('');
+
 
     const submitForm = function (e) {
-        e.preventDefault();
 
-        const formData = new FormData()
+        if (passwordInput === verify_passwordInput) {
 
-        formData.append('email', emailInput)
-        formData.append('password', passwordInput)
-        formData.append('name', name)
+            e.preventDefault();
+
+            const formData = new FormData()
+
+            formData.append('email', emailInput)
+            formData.append('password', passwordInput)
+            formData.append('name', name)
 
 
-        axios.post('http://localhost:8000/api/signup', formData)
-            .then(res => {
-                console.log(res.data)
-            })
+            axios.post('http://localhost:8000/api/signup', formData)
+                .then(res => {
+                    console.log(res.data)
+                })
+        } else {
+            e.preventDefault();
+
+            console.log('Passwords do not match');
+
+        }
     }
 
 
     return (
-        <div>
-            
+        <div className='app__signup'>
+
 
             <div className='app__signup__form'>
                 <form onSubmit={submitForm}>
+                    <h2>SIGN UP</h2>
+
 
                     <input type="text" name="name" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} />
 
@@ -38,11 +53,16 @@ const SignUp = () => {
 
                     <input type="password" name="password" placeholder="Password" value={passwordInput} onChange={(e) => setPasswordInput(e.target.value)} />
 
+                    <input type="password" name="verify_pass" placeholder="Verify Password" value={verify_passwordInput} onChange={(e) => setVerPasswordInput(e.target.value)} />
+
+                    <button type="submit" className='app__google__signup'><AiOutlineGoogle /></button>
 
                     <button type='submit'>Sign In</button>
 
+                    <p>Already Have An Account ? <a href='/signin'>Sign In</a></p>
 
                 </form>
+
             </div>
 
 
