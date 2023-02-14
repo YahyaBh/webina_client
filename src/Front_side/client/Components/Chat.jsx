c453f8f0edc8c1a5226ad26f795beafd3707d92d
Yahya Bouhsine <81886762+YahyaBh@users.noreply.github.com>
2023-02-14 22:38:07 +0100

    Chat

--- src/Front_side/client/Components/Chat.jsx
+++ src/Front_side/client/Components/Chat.jsx
@@ -1,6 +1,9 @@
+import Cookies from 'js-cookie';
 import Pusher from 'pusher-js';
-import React, { useEffect, useState } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import AuthUser from '../../AuthUser';
+import Navbar from '../../client/Components/Navbar'
+import { BiSend } from 'react-icons/bi';
 
 
 const Chat = () => {
@@ -12,28 +15,31 @@ const Chat = () => {
     const { http } = AuthUser();
 
     useEffect(() => {
+        document.getElementById('messages-container').scrollTop = document.getElementById('messages-container').scrollHeight;
 
+        const userData = new FormData();
 
+        userData.append('user_id', JSON.parse(Cookies.get('user')).id);
+        userData.append('user_token' , Cookies.get('token'));
+        userData.append('reciever_id' , '1');
 
-        http.post("/chat/messages")
+        
+
+        http.post("/chat/messages"  , userData)
             .then(res => {
-                setMessages(prevMessages => [...prevMessages, res.data.messages]);
-
-                for(let i = 0; i < res.data.messages.length; i++) {
-                    console.log(res.data.messages[i].id);
-                }
-                
+                setMessages(res.data.messages);
+                document.getElementById('messages-container').scrollTop = document.getElementById('messages-container').scrollHeight;
             })
 
-        // const pusher = new Pusher("56c79e77998c0788fbe2", {
-        //     cluster: "eu",
-        //     encrypted: true
-        // });
+        const pusher = new Pusher("56c79e77998c0788fbe2", {
+            cluster: "eu",
+            encrypted: true
+        });
 
-        // const channel = pusher.subscribe("WebIna");
-        // channel.bind("new-message", ({ message }) => {
-        //     setMessages(prevMessages => [...prevMessages, message]);
-        // });
+        const channel = pusher.subscribe("WebIna");
+        channel.bind('user-message', function (data) {
+            setMessages(data.message);
+        });
     }, []);
 
 
@@ -42,37 +48,54 @@ const Chat = () => {
 
 
 
-        // const messageData = new FormData();
+        const messageData = new FormData();
 
-        // messageData.append("message", input);
+        messageData.append("message", input);
+        messageData.append("user_id", JSON.parse(Cookies.get('user')).id);
+        messageData.append("user_token", Cookies.get('token'));
+        messageData.append("reciever_id", '1');
+        
 
-        // http.post("/chat/messages", messageData)
-        //     .then(res => {
-        //         if (res === 200) {
-        //             setMessages(prevMessages => [...prevMessages, res.data.messages]);
-        //         }
-        //     })
-        // setInput("");
+        http.post("/chat/message", messageData)
+            .then(res => {
+                document.getElementById('messages-container').scrollTop = document.getElementById('messages-container').scrollHeight;
+            })
+        setInput("");
+
+
+
+
 
     };
 
 
     return (
-        <>
-            <h1>Pusher Test</h1>
-            <p>
-                Try publishing an event to channel <code>my-channel</code>
-                with event name <code>my-event</code>.
-            </p>
+        <Fragment>
+            <div style={{ backgroundColor: '#000' }}>
+                <Navbar />
+            </div>
 
 
-            
+            <div className='chat-container'>
 
-            <form onSubmit={handleSubmit} >
-                <input type="text" value={input} placeholder='message' onChange={e => setInput(e.target.value)} />
-                <button type="submit"> SEND</button>
-            </form>
-        </>
+                <div className='chat-container-messages' id='messages-container'>
+                    {messages?.map((message, index) => (
+                        <div className={message.sender_id === JSON.parse(Cookies.get('user')).id ? 'sender_message' : 'reciever_message'} key={index + message.id}>
+                            <p>{message.message}</p>
+                        </div>
+                    ))}
+                </div>
+
+                <form onSubmit={handleSubmit} >
+                    <input type="text" value={input} placeholder='message' onChange={e => setInput(e.target.value)} />
+                    <button type="submit"><BiSend/></button>
+                </form>
+            </div>
+
+
+
+
+        </Fragment>
 
     )
 
